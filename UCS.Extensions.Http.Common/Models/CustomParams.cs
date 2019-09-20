fe392aa5358492da9dea6fe336621eed29f7dec4
4341 <a.boyko@ucs.ru>
2019-09-20 14:07:37 +0300

    [refactor] CustomParams as object store

--- UCS.Extensions.Http.Common/Models/CustomParams.cs
+++ UCS.Extensions.Http.Common/Models/CustomParams.cs
@@ -2,14 +2,19 @@
 
 namespace UCS.Extensions.Http.Common.Models
 {
-    public class CustomParams : Dictionary<string, string>
+    public class CustomParams : Dictionary<string, object>
     {
-        public void AddOrUpdate(string name, string value)
+        public void AddOrUpdate(string name, object value)
         {
-            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(value)) return;
+            if (string.IsNullOrEmpty(name) || value == null) return;
             if (ContainsKey(name)) Remove(name);
 
             Add(name, value);
         }
+
+        public T GetAsDef<T>(string key, T def = default)
+            where T : class
+                => TryGetValue(key, out var value) ? value as T ?? def : def;
+
     }
 }
