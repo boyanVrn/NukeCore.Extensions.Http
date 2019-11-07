200221f4f807107c487199a45eb365b09ad90bb3
4341 <a.boyko@ucs.ru>
2019-11-07 13:26:09 +0300

    [fix] add CreateFrom method in web response factory  interface

--- NukeCore.Extensions.Http.WebApi/IWebApiResponseFactory.cs
+++ NukeCore.Extensions.Http.WebApi/IWebApiResponseFactory.cs
@@ -1,4 +1,5 @@
-﻿using NukeCore.Extensions.Http.WebApi.Models.WebApiResponse;
+﻿using NukeCore.Extensions.Http.Models;
+using NukeCore.Extensions.Http.WebApi.Models.WebApiResponse;
 
 namespace NukeCore.Extensions.Http.WebApi
 {
@@ -6,5 +7,6 @@ namespace NukeCore.Extensions.Http.WebApi
     {
         IWebApiResponse Ok<T>(T data);
         IWebApiResponse Error<T>(T error);
+        IWebApiResponse CreateFrom<T>(IResponse<T> response);
     }
 }
