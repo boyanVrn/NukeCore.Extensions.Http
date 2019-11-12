62000761fcc3a8e87cb94910a14314012c189e7f
4341 <a.boyko@ucs.ru>
2019-11-12 13:21:27 +0300

    [new] add request base model

---
+++ NukeCore.Extensions.Http.Models/IRequest.cs
@@ -0,0 +1,6 @@
+﻿using NukeCore.Extensions.Http.Models.Base.Interfaces;
+
+namespace NukeCore.Extensions.Http.Models
+{
+    public interface IRequest<out T> : IData<T>, ISuccess { }
+}
---
+++ NukeCore.Extensions.Http.Models/RequestBase.cs
@@ -0,0 +1,21 @@
+﻿using NukeCore.Extensions.Http.Models.Base.Interfaces;
+
+namespace NukeCore.Extensions.Http.Models
+{
+    public class RequestBase<T> : IRequest<T>
+    {
+        private T _data;
+
+        public T Data
+        {
+            get => _data;
+            set
+            {
+                _data = value;
+                IsSuccess = value != null;
+            }
+        }
+
+        public bool IsSuccess { get; private set; }
+    }
+}
